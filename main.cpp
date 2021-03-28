2d9a24ba100f3d109a0f0d9d236d72189941ab7b
Pedro Figueiredo <up201904675@fe.up.pt>
2021-03-28 14:22:11 +0100

    Added main and test options

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include "gtest/gtest.h"
+
+using namespace std;
 
 int main(int argc, char **argv) {
-    std::cout << "Running all tests..." << std::endl;
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    cout << "Hello World!" << endl;
+    return 0;
 }
