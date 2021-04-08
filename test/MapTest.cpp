e56c6a02915d6c16c9fa5976cdc60e9dcc5f47d0
Pedro Figueiredo <up201904675@fe.up.pt>
2021-04-08 23:32:21 +0100

    Added a Test for Graph with 7 Points

--- test/MapTest.cpp
+++ test/MapTest.cpp
@@ -25,3 +25,39 @@ TEST(Map, test_dijkstra_super_simple) {
     checkAllPaths(myGraph, "P1<-P2|P2<-|");
     checkSinglePath(myGraph.getPath(P2, P1), "P2 P1 ");
 }
+
+TEST(Map, test_dijkstra_simple) {
+Graph myGraph;
+
+Point P1(0,1, "P1");
+Point P2(2,3, "P2");
+Point P3(2,0, "P3");
+Point P4(3,2, "P4");
+Point P5(4,3, "P5");
+Point P6(4,0, "P6");
+Point P7(5,1, "P7");
+
+myGraph.addVertex(P1);
+myGraph.addVertex(P2);
+myGraph.addVertex(P3);
+myGraph.addVertex(P4);
+myGraph.addVertex(P5);
+myGraph.addVertex(P6);
+myGraph.addVertex(P7);
+
+myGraph.addBidirectionalEdge(P1, P2, 1);
+myGraph.addBidirectionalEdge(P1, P3, 5);
+myGraph.addBidirectionalEdge(P2, P4, 2);
+myGraph.addBidirectionalEdge(P3, P4, 1);
+myGraph.addBidirectionalEdge(P3, P6, 1);
+myGraph.addBidirectionalEdge(P4, P5, 1);
+myGraph.addBidirectionalEdge(P4, P6, 5);
+myGraph.addBidirectionalEdge(P5, P7, 3);
+myGraph.addBidirectionalEdge(P6, P7, 1);
+
+myGraph.dijkstraShortestPath(P1);
+
+checkAllPaths(myGraph, "P1<-|P2<-P1|P3<-P4|P4<-P2|P5<-P4|P6<-P3|P7<-P6|");
+checkSinglePath(myGraph.getPath(P1, P7), "P1 P2 P4 P3 P6 P7 ");
+
+}
